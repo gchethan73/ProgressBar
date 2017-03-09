0f319c5dae4a0d174165928a1a0430017e36d385
Chethan Gurushanthappa <gchethan73@gmail.com>
2017-03-09 23:25:04 +0530

    Add files via upload

---
+++ Scripts/app/progressBars.js
@@ -0,0 +1,46 @@
+﻿
+var appModule = angular.module('progressBars', ['ngAnimate', 'ui.bootstrap']);
+
+appModule.controller('ProgressBarCtrl', function ($scope, $http) {
+    
+    init = function () {
+        $http({
+            method: 'GET',
+            url: 'https://pb-api.herokuapp.com/bars'
+        }).then(function(response) {
+            $scope.buttons = response.data.buttons;
+            $scope.bars = response.data.bars;
+            $scope.limit = response.data.limit;
+
+            initValues();
+
+        }, function(response) {
+            alert('An error occurred while getting the values.');
+        });
+    }
+    
+    function initValues() {
+        $scope.barArray = [];
+        angular.forEach($scope.bars, function (value, key) {
+            $scope.barArray.push({ dropDownText: 'Progress-' + key, dropDownValue: value, index: key });
+        });
+
+        $scope.selectedProgressBar = $scope.barArray[0];
+    }
+
+    $scope.ChangeValue = function (value) {
+        var currentValue = $scope.bars[$scope.selectedProgressBar.index];
+        if ((currentValue + value) > 0)
+        {
+            $scope.bars[$scope.selectedProgressBar.index] += value;
+        }
+        else
+        {
+            $scope.bars[$scope.selectedProgressBar.index] = 0;
+        }
+    }
+
+    init();
+
+
+});
